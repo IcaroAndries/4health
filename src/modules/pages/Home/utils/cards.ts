e13c05ad6566ba5281f2108f36946d63d65ea5eb
IcaroAndries <icaro.dev.andries@gmail.com>
2024-03-02 15:08:02 -0300

    fixing interface name

--- src/modules/pages/Home/utils/cards.ts
+++ src/modules/pages/Home/utils/cards.ts
@@ -1,10 +1,10 @@
-interface IHomeCards {
+interface IHomeCard {
   title: string
   description: string
   image?: string
 }
 
-export const homeCards: IHomeCards[] = [
+export const homeCards: IHomeCard[] = [
   {
     title: 'About',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
