6d295d93f9a002726b2f4116053d4e94c6db8085
IcaroAndries <icaro.dev.andries@gmail.com>
2024-03-05 10:31:56 -0300

    new sidebard style

--- src/modules/templates/MainTemplate/elements/SideBar/index.tsx
+++ src/modules/templates/MainTemplate/elements/SideBar/index.tsx
@@ -1,33 +1,62 @@
 'use client';
 
 import {
+  IconButton,
   Image,
   Stack,
   Text,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
 
 import useResponsiveSize from 'modules/_shared/hooks/ResponsiveSize';
 
 const SideBar: React.FC = () => {
   const { vw } = useResponsiveSize();
+  const trasitionTime = '0.8s';
 
-  const [colapsed, setCollapsed] = useState<boolean>(false);
+  const [colapsed, setCollapsed] = useState<boolean>(true);
 
   return (
     <Stack
       w="100%"
       h={colapsed ? 'auto' : '100%'}
+      maxH={colapsed ? '80vh' : '100%'}
+      mt={colapsed ? '10vh' : '0px'}
       bgColor="blackAlpha.900"
       spacing="0px"
-      p={vw('10px')}
+      p={colapsed ? vw('5px') : vw('10px')}
+      position="relative"
+      transition={trasitionTime}
+      borderRadius="0px 10px 10px 0px"
     >
+      <IconButton
+        isRound
+        zIndex={5}
+        bgColor="yellow.400"
+        textColor="whiteAlpha.900"
+        icon={
+          colapsed
+            ? <IoIosArrowForward />
+            : <IoIosArrowBack />
+        }
+        onClick={() => setCollapsed(!colapsed)}
+        aria-label="Toggle SideBar"
+        position="absolute"
+        w={colapsed ? '30px' : '22px'}
+        minW={colapsed ? '30px' : '22px'}
+        h={colapsed ? '30px' : '22px'}
+        top={colapsed ? '50%' : 'calc(10px + 3em)'}
+        right={colapsed ? '-15px' : '-11px'}
+        transition={trasitionTime}
+      />
       <Stack
         p={vw('15px')}
         spacing={vw('8px')}
         align="center"
         justify="center"
         direction="row"
+        transition={trasitionTime}
       >
         <Image
           src="/light.png"
@@ -35,16 +64,24 @@ const SideBar: React.FC = () => {
           alt="4HealthLogo"
         />
 
-        <Text
-          fontSize="3em"
-          textColor="yellow.400"
-          fontWeight="200"
-          borderRadius="0px 10px 10px 0px"
-        >
-          4Health
-        </Text>
+        {
+          !colapsed && (
+            <Text
+              fontSize="3em"
+              textColor="yellow.400"
+              fontWeight="200"
+              borderRadius="0px 10px 10px 0px"
+            >
+              4Health
+            </Text>
+          )
+        }
       </Stack>
-      <Stack />
+      <Stack>
+        {
+          // the list goes here
+        }
+      </Stack>
     </Stack>
   );
 };
