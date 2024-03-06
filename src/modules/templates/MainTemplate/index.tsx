af9ff56f7db375f23367fdcb58a2fd1b7598a0d9
IcaroAndries <icaro.dev.andries@gmail.com>
2024-03-06 02:10:24 -0300

    feat: adding topMenu

--- src/modules/templates/MainTemplate/index.tsx
+++ src/modules/templates/MainTemplate/index.tsx
@@ -1,25 +1,33 @@
 import { Grid, Stack } from '@chakra-ui/react';
 import React from 'react';
 
+import useResponsiveSize from 'modules/_shared/hooks/use-responsive-size';
+
 import SideBar from './elements/SideBar';
+import TopMenu from './elements/TopMenu';
 
-const MainTemplate: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <Grid
-    w="100%"
-    minH="100vw"
-    bgColor="whiteAlpha.400"
-    gridTemplateColumns="auto 1fr"
-  >
-    <SideBar />
-    <Stack
+const MainTemplate: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { vw } = useResponsiveSize();
+
+  return (
+    <Grid
       w="100%"
-      h="100%"
-      align="center"
-      justify="center"
+      minH="100vw"
+      bgColor="whiteAlpha.400"
+      gridTemplateColumns="auto 1fr"
     >
-      {children}
-    </Stack>
-  </Grid>
-);
-
+      <SideBar />
+      <Stack
+        w="100%"
+        h="100%"
+        align="center"
+        justify="center"
+        spacing={vw('60px')}
+      >
+        <TopMenu />
+        {children}
+      </Stack>
+    </Grid>
+  );
+};
 export default MainTemplate;
